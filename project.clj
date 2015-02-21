1d796788bde1639eff021239b93194017ccb0be7
Mattia Gheda <ghedamat@gmail.com>
2015-02-21 16:44:27 -0500

    version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject reagent-react-router "0.1.1-SNAPSHOT"
+(defproject reagent-react-router "0.1.0-0"
   :description "Reagent wrapper for the react-router "
   :url "https://github.com/ghedamat/reagent-react-router"
   :clojurescript? true
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies  [[org.clojure/clojure "1.6.0"]
-                  [cljsjs/react-router "0.12.0"]
+                  [cljsjs/react-router "0.12.0-0"]
                   [reagent "0.5.0-alpha3"]]
   :plugins  [[codox "0.6.4"]]
   :profiles  {:dev
